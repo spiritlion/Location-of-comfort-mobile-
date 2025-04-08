855a31f41c0ff09caf08a838b627334633cb36b6
Артём <fed-cat@bk.ru>
2025-04-08 23:09:21 +0300

    add map

--- app/src/main/java/com/example/locationofcomfort/MainActivity.kt
+++ app/src/main/java/com/example/locationofcomfort/MainActivity.kt
@@ -11,21 +11,75 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.locationofcomfort.custom.screens.MainScreen
 import com.example.locationofcomfort.ui.theme.LocationOfComfortTheme
-import kotlinx.coroutines.MainScope
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.mapview.MapView
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var mapView: MapView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        MapKitFactory.setApiKey("697a8f69-8ca4-4034-8d90-f2e21b4b4ca5")
+        MapKitFactory.initialize(this)
+        mapView = MapView(this).apply {
+            map.move(
+                CameraPosition(
+                    Point(
+                        56.999799,
+                        40.973014
+                    ),
+                    11.0f,
+                    0.0f,
+                    0.0f
+                )
+            )
+        }
+
+        setContent {
+            LocationOfComfortTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
+                    MainScreen(modifier = Modifier.padding(paddingValues), mapView)
+                }
+            }
+        }
+        Animation(Animation.Type.SMOOTH, 300f)
+
         enableEdgeToEdge()
         setContent {
             LocationOfComfortTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) {
-                    MainScreen(modifier = Modifier.padding(it))
+                    MainScreen(modifier = Modifier.padding(it), mapView = mapView)
                 }
             }
         }
+    }
+
+    override fun onStop() {
+        mapView.onStop()
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
+    }
+
+    override fun onStart() {
+        mapView.onStart()
+        MapKitFactory.getInstance().onStart()
+        super.onStart()
+    }
+
+
+}
+
+@Composable
+fun MapViewContainer(mapView: MapView) {
+    // Здесь можно использовать AndroidView для интеграции MapView в Compose
+    AndroidView(factory = { mapView }) { view ->
+        // Дополнительные настройки карты можно делать здесь
     }
 }
 
--- app/src/main/java/com/example/locationofcomfort/custom/screens/MainScreen.kt
+++ app/src/main/java/com/example/locationofcomfort/custom/screens/MainScreen.kt
@@ -1,43 +1,37 @@
 package com.example.locationofcomfort.custom.screens
 
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.launch
+import com.example.locationofcomfort.MapViewContainer
+import com.yandex.mapkit.mapview.MapView
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
+fun MainScreen(modifier: Modifier = Modifier, mapView: MapView) {
     var drawerState by remember { mutableStateOf(DrawerValue.Closed) }
-    val drawerWidth = 300.dp // Ширина вашего Drawer
+    val drawerWidth = 150.dp // Ширина вашего Drawer
     Column(modifier) {
         ModalNavigationDrawer(
             drawerState = rememberDrawerState(initialValue = drawerState),
@@ -53,16 +47,20 @@ fun MainScreen(modifier: Modifier = Modifier) {
                 }
             },
             content = {
-                Box(modifier = Modifier.fillMaxSize()) {
-                    Button(onClick = {
-                        drawerState = if (drawerState == DrawerValue.Open) {
-                            DrawerValue.Closed
-                        } else {
-                            DrawerValue.Open
-                        }
-                    }) {
-                        Text("Toggle Drawer")
-                    }
+                Row {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .clickable {
+                                drawerState = if (drawerState == DrawerValue.Open) {
+                                    DrawerValue.Closed
+                                } else {
+                                    DrawerValue.Open
+                                }
+                            }
+                    )
+                    MapViewContainer(mapView)
                 }
             },
             scrimColor = Color.Transparent // Убираем затемнение
